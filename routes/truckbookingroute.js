d07da36a2c2893fcda4f154236ad0a91d0e8a26a
Muhammad Haseeb <151600808+M-Haseeb138@users.noreply.github.com>
2025-05-06 11:37:05 +0500

    logut Done

--- routes/truckbookingroute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const  truckBookingController = require("../Controllers/truckbookingController");
-const verifyToken =require("../Middlewares/authMiddleware");
-
-router.post("/booktruck",verifyToken,truckBookingController.bookTrucks);
-router.get("/recentBookings", truckBookingController.getRecentBookings);
-
-module.exports= router;
