9fe62ec11d7f8f8fce672da6db04d26267169a4c
Muhammad Haseeb <151600808+M-Haseeb138@users.noreply.github.com>
2025-05-30 13:10:43 +0500

    token set

--- routes/trackingRoutes.js
+++ routes/trackingRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const  verifyToken  = require("../Middlewares/authMiddleware");
+const verifyAdmin = require("../Middlewares/adminAuthMiddleware");
 const trackingController = require("../Controllers/trackingController");
 console.log("TrackingController:", trackingController);
 
 router.get('/:bookingId/history', verifyToken, trackingController.getLocationHistory);
-router.get('/bookingstatus/:bookingId', verifyToken, trackingController.getBookingStatus);
+router.get('/bookingstatus/:bookingId',verifyAdmin, trackingController.getBookingStatus);
 
 module.exports = router;
